0bd8b565d34208e71d2e724f124b756926afb09d
MizanRbf <mizanrbf@gmail.com>
2025-12-27 10:31:58 +0600

    vehicles all apis updated

--- src/modules/vehicles/vehicle.routes.ts
+++ src/modules/vehicles/vehicle.routes.ts
@@ -3,14 +3,19 @@ import { vehicleControllers } from "./vehicle.controller";
 import auth from "../../middleware/auth";
 const router = express.Router();
 
+// Post vehicles
 router.post("/", vehicleControllers.createVehicle);
 
+// Get all vehicles
 router.get("/", vehicleControllers.getVehicle);
 
+// Get single vehicles
 router.get("/:vehicleId", vehicleControllers.getSingleVehicle);
 
+// Update vehicles
 router.put("/:vehicleId", auth("admin"), vehicleControllers.updateVehicle);
 
+// Delete vehicles
 router.delete("/:vehicleId", auth("admin"), vehicleControllers.deleteVehicle);
 
 export const vehicleRoutes = router;
