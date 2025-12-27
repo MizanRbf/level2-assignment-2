f4a78ca663fcdcd7f7ae263a5ffb0ade0d81f902
MizanRbf <mizanrbf@gmail.com>
2025-12-27 18:37:12 +0600

    bookings post apis created successfully

--- src/modules/bookings/booking.service.ts
+++ src/modules/bookings/booking.service.ts
@@ -1,11 +1,25 @@
 import { pool } from "../../config/db";
 
 // Create Bookings
-const createBooking = async (payload: Record<string, unknown>) => {
-  const { rent_start_date, rent_end_date, total_price, status } = payload;
+const createBooking = async (payload: any) => {
+  const {
+    customer_id,
+    vehicle_id,
+    rent_start_date,
+    rent_end_date,
+    total_price,
+    status,
+  } = payload;
   const result = await pool.query(
-    `INSERT INTO bookings(rent_start_date,rent_end_date,total_price,status) VALUES($1,$2,$3,$4) RETURNING *`,
-    [rent_start_date, rent_end_date, total_price, status]
+    `INSERT INTO bookings(customer_id,vehicle_id,rent_start_date,rent_end_date,total_price,status) VALUES($1,$2,$3,$4,$5,$6) RETURNING *`,
+    [
+      customer_id,
+      vehicle_id,
+      rent_start_date,
+      rent_end_date,
+      total_price,
+      status,
+    ]
   );
   return result;
 };
